21c298fb79e9bbd9ca0d9641090223a87b4cd1f9
Vitória Rafaelly Diógenes <49590540+vitoriarafaelly@users.noreply.github.com>
2021-06-16 17:34:04 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -0,0 +1,53 @@
+package br.com.alura.forum.controller.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+
+import br.com.alura.forum.modelo.Topico;
+
+public class TopicoDto {
+	
+	private Long id;
+	private String titulo;
+	private String mensagem;
+	private LocalDateTime data;
+	
+	public TopicoDto(Topico topico) {
+		this.id = topico.getId();
+		this.titulo = topico.getTitulo();
+		this.mensagem = topico.getMensagem();
+		this.data = topico.getDataCriacao();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public LocalDateTime getData() {
+		return data;
+	}
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+	public static Page<TopicoDto> converter (Page<Topico> topicos){
+		return topicos.map(TopicoDto::new);
+	}
+
+}
